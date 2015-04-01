1ab6e74544ae338a841f56fe874e7c97a280e5ea
Salvatore Avanzo <salvatore.avanzo@gmail.com>
2015-04-01 15:01:30 +0200

    fix broken url

--- django_interfaces_design/settings.py
+++ django_interfaces_design/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
     'bootstrap3',
     'formsparty'
 )
@@ -108,3 +109,5 @@ TEMPLATE_DIRS = (
 )
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+SITE_ID = 1
--- django_interfaces_design/urls.py
+++ django_interfaces_design/urls.py
@@ -1,7 +1,16 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.contrib.sites.models import Site
 
-urlpatterns = patterns('',
-    url(r'', include('formsparty.urls', namespace='formsparty')),
-    url(r'^admin/', include(admin.site.urls)),
-)
+current_site = Site.objects.get_current()
+
+if 'herokuapp' in current_site.domain:
+    urlpatterns = patterns('',
+        url(r'', include('formsparty.urls', namespace='formsparty')),
+        url(r'^adomino/', include(admin.site.urls)),
+    )
+else:
+    urlpatterns = patterns('',
+        url(r'', include('formsparty.urls', namespace='formsparty')),
+        url(r'^admin/', include(admin.site.urls)),
+    )
