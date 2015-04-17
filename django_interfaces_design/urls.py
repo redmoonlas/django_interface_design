f2bf955a211ec59c19c45f22f8bc62e1cc45909a
Salvatore Avanzo <salvatore.avanzo@gmail.com>
2015-04-17 16:34:22 +0200

    restored urls

--- django_interfaces_design/urls.py
+++ django_interfaces_design/urls.py
@@ -2,16 +2,17 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.contrib.sites.models import Site
 
-current_site = Site.objects.get_current()
+# current_site = Site.objects.get_current()
+#
+# if 'herokuapp' in current_site.domain:
+#     urlpatterns = patterns('',
+#         url(r'', include('formsparty.urls', namespace='formsparty')),
+#         url(r'^adomino/', include(admin.site.urls)),
+#     )
+# else:
 
-if 'herokuapp' in current_site.domain:
-    urlpatterns = patterns('',
-        url(r'', include('formsparty.urls', namespace='formsparty')),
-        url(r'^adomino/', include(admin.site.urls)),
-    )
-else:
-    urlpatterns = patterns('',
-        url(r'', include('formsparty.urls', namespace='formsparty')),
-        url(r'^cs/', include('coffescript.urls', namespace='coffescript')),
-        url(r'^admin/', include(admin.site.urls)),
-    )
+urlpatterns = patterns('',
+    url(r'', include('formsparty.urls', namespace='formsparty')),
+    url(r'^cs/', include('coffescript.urls', namespace='coffescript')),
+    url(r'^admin/', include(admin.site.urls)),
+)
